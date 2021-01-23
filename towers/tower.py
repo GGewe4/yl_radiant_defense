58f31210f1b2672f6b0d21283a3db137976d31b8
RoketFlame <danil.slinchuk@gmail.com>
2021-01-23 17:13:24 +0300

    add comments

--- towers/tower.py
+++ towers/tower.py
@@ -5,6 +5,7 @@ import pygame
 towers_sprites = pygame.sprite.Group()
 
 
+# function collide rect and circle
 def intersects(rect, r, center):
     circle_distance_x = abs(center[0] - rect.centerx)
     circle_distance_y = abs(center[1] - rect.centery)
@@ -21,13 +22,16 @@ def intersects(rect, r, center):
 class Tower(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__(towers_sprites)
+        # property rect
         self.x = x
         self.y = y
         self.width = 0
         self.height = 0
+        # prices
         self.sell_price = [0, 0, 0]
         self.price = [0, 0, 0]
         self.level = 0
+        # base attributes
         self.selected = True
         self.main_img = None
         self.damage = 1
@@ -37,13 +41,8 @@ class Tower(pygame.sprite.Sprite):
         self.is_attack = False
         self.splash = False
 
-    def upgrade(self):
-        pass
-
-    def click(self):
-        pass
-
     def attack(self, enemies):
+        # try to attack enemies
         add_money = 0
         if self.is_attack:
             enemy_closest = []
@@ -67,6 +66,7 @@ class Tower(pygame.sprite.Sprite):
         return add_money
 
     def update(self, enemies):
+        # draw animation
         for enemy in enemies:
             if intersects(enemy.hit_box, self.range, (self.x, self.y)):
                 self.is_attack = True
@@ -77,8 +77,9 @@ class Tower(pygame.sprite.Sprite):
             self.image = self.main_img
 
     def draw_radius(self, win):
+        # draw attack radius
         surface = pygame.Surface((self.range * 2, self.range * 2), pygame.SRCALPHA)
-        pygame.draw.circle(surface, (128, 128, 128,  120), (self.range, self.range), self.range, 0)
+        pygame.draw.circle(surface, (128, 128, 128, 120), (self.range, self.range), self.range, 0)
         win.blit(surface, (self.x - self.range, self.y - self.range))
 
     def collide(self, wind, x, y):
@@ -86,6 +87,7 @@ class Tower(pygame.sprite.Sprite):
         return self.rect.collidepoint(x, y)
 
 
+# base func
 def load_image(name, colorkey=None):
     pygame.init()
     fullname = os.path.join(name)
